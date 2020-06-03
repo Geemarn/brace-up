d813ae0b4dbf25d96bbfff34c8493c617395fc38
A.Adegoke Jnr <adeg4ife@gmail.com>
2020-06-03 10:22:23 +0100

    2nd push to heroku

--- server.js
+++ server.js
@@ -35,12 +35,13 @@ app.use("/api/users", users);
 app.use("/api/todos", todos);
 
 /////ready for production/////
-app.use(express.static(path.join(__dirname, "build")));
 //server static asset if in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
